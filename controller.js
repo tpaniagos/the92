aee1d0187b96d607326587a6f91e8d6bf859160a
tpaniagos <thiago_paniago@hotmail.com>
2017-02-01 14:58:42 -0300

    Add files via upload

---
+++ controller.js
@@ -0,0 +1,15 @@
+var ListController = function ($scope) {
+
+       function Enter(event){
+
+              if(event.keycode == 13){
+
+                     Console.log('555')
+              }
+
+       }
+};
+//criacao do controller etapa 01
+
+app.controller("ListController", ListController);
+//vinculacao do modulo com controller etapa 02
